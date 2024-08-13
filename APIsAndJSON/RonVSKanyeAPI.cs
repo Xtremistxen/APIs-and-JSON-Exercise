2f0055c1e5cb633ea677cd4988fc3f1744f050d7
Robert Montgomery <boobyvxvbooby_gmail.com>
2024-08-13 11:47:36 -0500

    First exercise pushed up will now work on exercise2

--- APIsAndJSON/RonVSKanyeAPI.cs
+++ APIsAndJSON/RonVSKanyeAPI.cs
@@ -2,11 +2,39 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Nodes;
+using Newtonsoft.Json.Linq;
 using System.Threading.Tasks;
 
 namespace APIsAndJSON
 {
     internal class RonVSKanyeAPI
     {
+        public static void RonQuote()
+        {
+            var client = new HttpClient();
+            
+            var RonURL = "https://ron-swanson-quotes.herokuapp.com/v2/quotes";
+
+            var ronResponse = client.GetStringAsync(RonURL).Result;
+
+            var ronQuote = JArray.Parse(ronResponse);
+
+            Console.WriteLine($"Ron: {ronQuote[0]}\n");
+        }
+
+        public static void KanyeQuote()
+        {
+            var client = new HttpClient();
+            
+            var kanyeURL = "https://api.kanye.rest";
+
+            var kanyeResponse = client.GetStringAsync(kanyeURL).Result;
+
+            var kanyeQuote = JObject.Parse(kanyeResponse).GetValue("quote").ToString();
+
+            Console.WriteLine($"Kanye: {kanyeQuote}\n");
+        }
     }
 }
+
